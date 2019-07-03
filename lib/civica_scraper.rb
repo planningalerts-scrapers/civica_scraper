3e8e710d0b5538cfab6ccf00ee98c5f8d1b6bf20
Matthew Landauer <mlandauer@gmail.com>
2019-07-03 17:16:58 +1000

    Add wollondilly

--- lib/civica_scraper.rb
+++ lib/civica_scraper.rb
@@ -1,15 +1,19 @@
 require "civica_scraper/version"
 
 require "civica_scraper/authority/burwood"
+require "civica_scraper/authority/wollondilly"
 
 module CivicaScraper
   AUTHORITIES = {
-    burwood: {}
+    burwood: {},
+    wollondilly: {}
   }
 
   def self.scrape_and_save(authority)
     if authority == :burwood
       Authority::Burwood.scrape_and_save
+    elsif authority == :wollondilly
+      Authority::Wollondilly.scrape_and_save
     else
       raise "Unknown authority: #{authority}"
     end
