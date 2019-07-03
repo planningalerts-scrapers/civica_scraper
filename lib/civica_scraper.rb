5e88a835c5fbebb313255df82b47241ebec08acc
Matthew Landauer <mlandauer@gmail.com>
2019-07-03 17:34:59 +1000

    Add rubocop

--- lib/civica_scraper.rb
+++ lib/civica_scraper.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
 require "civica_scraper/version"
 
 require "civica_scraper/authority/burwood"
 require "civica_scraper/authority/wollondilly"
 require "civica_scraper/authority/woollahra"
 
+# Scrape civica websites
 module CivicaScraper
   AUTHORITIES = {
     burwood: {},
     wollondilly: {},
     woollahra: {}
-  }
+  }.freeze
 
   def self.scrape_and_save(authority)
     if authority == :burwood
