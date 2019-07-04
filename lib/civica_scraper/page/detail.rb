00de335998729e37dc67cab6c64942f3e5f8eb24
Matthew Landauer <mlandauer@gmail.com>
2019-07-04 13:30:46 +1000

    Extract method

--- lib/civica_scraper/page/detail.rb
+++ lib/civica_scraper/page/detail.rb
@@ -5,22 +5,20 @@ module CivicaScraper
     # A page with all the information (hopefully) about a single
     # development application
     module Detail
+      def self.parse_date(string)
+        Date.strptime(string, "%d/%m/%Y")
+      rescue ArgumentError
+        puts "Invalid date: #{string}"
+        nil
+      end
+
       def self.scrape(doc)
         rows = doc.search(".rowDataOnly > .inputField:nth-child(2)").map { |e| e.inner_text.strip }
-        reference = rows[2]
-        begin
-          date_received = Date.strptime(rows[3], "%d/%m/%Y").to_s
-        rescue ArgumentError
-          date_received = nil
-        end
-
-        puts "Invalid date: #{rows[3].inspect}" unless date_received
-
         {
-          council_reference: reference,
+          council_reference: rows[2],
           address: rows[0],
           description: rows[1],
-          date_received: date_received
+          date_received: parse_date(rows[3])&.to_s
         }
       end
     end
