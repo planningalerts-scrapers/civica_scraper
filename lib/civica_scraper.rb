a61458ace3753eb6c935669f4ad16867be2756c2
Matthew Landauer <mlandauer@gmail.com>
2019-07-04 15:12:48 +1000

    Extract method

--- lib/civica_scraper.rb
+++ lib/civica_scraper.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "civica_scraper/authority/burwood"
-require "civica_scraper/authority/wollondilly"
 require "civica_scraper/authority/woollahra"
 require "civica_scraper/page/detail"
 require "civica_scraper/page/index"
@@ -21,13 +19,42 @@ module CivicaScraper
 
   def self.scrape_and_save(authority)
     if authority == :burwood
-      Authority::Burwood.scrape_and_save
+      CivicaScraper.scrape_and_save_period(
+        # TODO: Get this weird url (with a nodeNum whatever that is) by following
+        # a link from a more consistent url
+        "https://ecouncil.burwood.nsw.gov.au/eservice/daEnquiryInit.do?doc_typ=10&nodeNum=219",
+        Date.today - 7,
+        Date.today
+      )
     elsif authority == :wollondilly
-      Authority::Wollondilly.scrape_and_save
+      CivicaScraper.scrape_and_save_period(
+        "https://ecouncil.wollondilly.nsw.gov.au/eservice/daEnquiryInit.do?nodeNum=40801",
+        Date.today - 7,
+        Date.today
+      )
     elsif authority == :woollahra
       Authority::Woollahra.scrape_and_save
     else
       raise "Unknown authority: #{authority}"
+    end
+  end
+
+  def self.scrape_and_save_period(base_url, date_from, date_to)
+    agent = Mechanize.new
+    page = agent.get(base_url)
+    page = Page::Search.period(page, date_from, date_to)
+
+    Page::Index.scrape(page) do |record|
+      CivicaScraper.save(
+        "council_reference" => record[:council_reference],
+        "address" => record[:address],
+        "description" => record[:description],
+        # We can't give a link directly to an application.
+        # Bummer. So, giving link to the search page
+        "info_url" => base_url,
+        "date_received" => record[:date_received],
+        "date_scraped" => Date.today.to_s
+      )
     end
   end
 
