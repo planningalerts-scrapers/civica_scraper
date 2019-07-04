a7b85b8fa4ed4e47a17629d14f8e140fd3ca94db
Matthew Landauer <mlandauer@gmail.com>
2019-07-04 15:00:56 +1000

    Now that we're allowing the scraper to raise an exception more easily don't skip on an empty field in a record. Allow PA itself to do that.

--- lib/civica_scraper/authority/wollondilly.rb
+++ lib/civica_scraper/authority/wollondilly.rb
@@ -1,10 +1,6 @@
 module CivicaScraper
   module Authority
     module Wollondilly
-      def self.has_blank?(record)
-        record.values.any?{|v| v.nil? || v.length == 0}
-      end
-
       def self.scrape_and_save
         base_url = "https://ecouncil.wollondilly.nsw.gov.au/eservice/daEnquiryInit.do?nodeNum=40801"
 
@@ -17,18 +13,14 @@ module CivicaScraper
         formpage = Page::Search.period(datepage, date_from, date_to)
 
         Page::Index.scrape(formpage) do |record|
-          unless has_blank?(record)
-            CivicaScraper.save(
-              'council_reference' => record[:council_reference],
-              'address' => record[:address],
-              'description' => record[:description],
-              'info_url' => base_url,
-              'date_scraped' => Date.today.to_s,
-              'date_received' => record[:date_received]
-            )
-          else
-            puts "Something not right here: #{record}"
-          end
+          CivicaScraper.save(
+            'council_reference' => record[:council_reference],
+            'address' => record[:address],
+            'description' => record[:description],
+            'info_url' => base_url,
+            'date_scraped' => Date.today.to_s,
+            'date_received' => record[:date_received]
+          )
         end
       end
     end
