f4102f6575af5ea6bf4ead5b0bebca056d463c1b
Matthew Landauer <mlandauer@gmail.com>
2019-07-04 10:35:46 +1000

    Extract method

--- lib/civica_scraper/authority/burwood.rb
+++ lib/civica_scraper/authority/burwood.rb
@@ -18,6 +18,15 @@ module CivicaScraper
         CivicaScraper.save(record)
       end
 
+      # For the being just returns the urls of the detail pages
+      def self.scrape_index_page(page, search_result_url)
+        (0..page.search('.non_table_headers').size - 1).each do |i|
+          yield(
+            url: search_result_url + i.to_s
+          )
+        end
+      end
+
       def self.scrape_and_save
         date_from = Date.today - 7
         date_to = Date.today
@@ -31,8 +40,9 @@ module CivicaScraper
 
         page = Page::Search.period(page, date_from, date_to)
 
-        (0..page.search('.non_table_headers').size - 1).each do |i|
-          doc = agent.get(search_result_url + i.to_s)
+        scrape_index_page(page, search_result_url) do |record|
+          # Just use the url for the time being
+          doc = agent.get(record[:url])
           scrape_table(doc, general_search_url)
         end
       end
