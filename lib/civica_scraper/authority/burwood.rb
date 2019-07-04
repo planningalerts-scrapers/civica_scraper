37676de29d10edc865373d1634cf26c74b30396b
Matthew Landauer <mlandauer@gmail.com>
2019-07-04 15:02:44 +1000

    Tiny refactor

--- lib/civica_scraper/authority/burwood.rb
+++ lib/civica_scraper/authority/burwood.rb
@@ -2,17 +2,14 @@ module CivicaScraper
   module Authority
     module Burwood
       def self.scrape_and_save
+        # TODO: Get this weird url (with a nodeNum whatever that is) by following
+        # a link from a more consistent url
+        base_url = 'https://ecouncil.burwood.nsw.gov.au/eservice/daEnquiryInit.do?doc_typ=10&nodeNum=219'
         date_from = Date.today - 7
         date_to = Date.today
 
-        # TODO: Get this weird url (with a nodeNum whatever that is) by following
-        # a link from a more consistent url
-        general_search_url = 'https://ecouncil.burwood.nsw.gov.au/eservice/daEnquiryInit.do?doc_typ=10&nodeNum=219'
-
-        # Grab the starting page and go into each link to get a more reliable data format.
         agent = Mechanize.new
-        page = agent.get(general_search_url)
-
+        page = agent.get(base_url)
         page = Page::Search.period(page, date_from, date_to)
 
         Page::Index.scrape(page) do |record|
@@ -21,7 +18,7 @@ module CivicaScraper
             'address' => record[:address],
             'description' => record[:description],
             # We can't give a link directly to an application. Bummer. So, giving link to the search page
-            'info_url' => general_search_url,
+            'info_url' => base_url,
             'date_received' => record[:date_received],
             'date_scraped' => Date.today.to_s
           )
--- lib/civica_scraper/authority/wollondilly.rb
+++ lib/civica_scraper/authority/wollondilly.rb
@@ -3,16 +3,14 @@ module CivicaScraper
     module Wollondilly
       def self.scrape_and_save
         base_url = "https://ecouncil.wollondilly.nsw.gov.au/eservice/daEnquiryInit.do?nodeNum=40801"
-
         date_from = Date.today - 7
         date_to = Date.today
 
         agent = Mechanize.new
-        datepage = agent.get(base_url)
+        page = agent.get(base_url)
+        page = Page::Search.period(page, date_from, date_to)
 
-        formpage = Page::Search.period(datepage, date_from, date_to)
-
-        Page::Index.scrape(formpage) do |record|
+        Page::Index.scrape(page) do |record|
           CivicaScraper.save(
             'council_reference' => record[:council_reference],
             'address' => record[:address],
