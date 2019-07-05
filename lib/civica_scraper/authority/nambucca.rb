b72fd3caf86f3dd231f61201211fc5614e72b19f
Matthew Landauer <mlandauer@gmail.com>
2019-07-06 01:16:52 +1000

    Remove support for MORPH_PERIOD

--- lib/civica_scraper/authority/nambucca.rb
+++ lib/civica_scraper/authority/nambucca.rb
@@ -5,31 +5,10 @@ module CivicaScraper
   module Authority
     module Nambucca
       def self.scrape_and_save
-        case ENV['MORPH_PERIOD']
-          when 'lastmonth'
-            date = Date.today.prev_month
-          	dateFrom = Date.new(date.year, date.month, 1).strftime('%d/%m/%Y')
-          	dateTo   = Date.new(date.year, date.month, -1).strftime('%d/%m/%Y')
-          when 'thismonth'
-            date = Date.today
-          	dateFrom = Date.new(date.year, date.month, 1).strftime('%d/%m/%Y')
-          	dateTo   = Date.new(date.year, date.month, -1).strftime('%d/%m/%Y')
-          else
-            unless ENV['MORPH_PERIOD'].nil?
-              matches = ENV['MORPH_PERIOD'].scan(/^(19[0-9]{2}|20[0-9]{2})$/)
-            end
-            unless matches.nil?
-              dateFrom = Date.new(matches[0][0].to_i, 1, 1).strftime('%d/%m/%Y')
-              dateTo   = Date.new(matches[0][0].to_i, 12, 31).strftime("%d/%m/%Y")
-            else
-              ENV['MORPH_PERIOD'] = 'thisweek'
-              date = Date.today
-              dateTo   = Date.new(date.year, date.month, date.day).strftime('%d/%m/%Y')
-              date = Date.today - 10
-              dateFrom = Date.new(date.year, date.month, date.day).strftime('%d/%m/%Y')
-            end
-        end
-        puts "Getting data in `" + ENV['MORPH_PERIOD'] + "`, changable via MORPH_PERIOD environment"
+        date = Date.today
+        dateTo   = Date.new(date.year, date.month, date.day).strftime('%d/%m/%Y')
+        date = Date.today - 10
+        dateFrom = Date.new(date.year, date.month, date.day).strftime('%d/%m/%Y')
 
         comment_url = 'mailto:council@nambucca.nsw.gov.au'
         info_url = 'https://eservices.nambucca.nsw.gov.au/eservice/daEnquiryInit.do?doc_typ=10&nodeNum=2811'
