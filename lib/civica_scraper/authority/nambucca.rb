4762b44b1fdd754bf15a011edad14dde4d97f612
Matthew Landauer <mlandauer@gmail.com>
2019-07-06 01:23:47 +1000

    Small refactor

--- lib/civica_scraper/authority/nambucca.rb
+++ lib/civica_scraper/authority/nambucca.rb
@@ -2,20 +2,19 @@ module CivicaScraper
   module Authority
     module Nambucca
       def self.scrape_and_save
-        date = Date.today
-        dateTo   = Date.new(date.year, date.month, date.day).strftime('%d/%m/%Y')
-        date = Date.today - 10
-        dateFrom = Date.new(date.year, date.month, date.day).strftime('%d/%m/%Y')
+        date_from = Date.today - 10
+        date_to = Date.today
 
         info_url = 'https://eservices.nambucca.nsw.gov.au/eservice/daEnquiryInit.do?doc_typ=10&nodeNum=2811'
 
         # Grab the starting page and put in the date and her we go
         agent = Mechanize.new
         page = agent.get(info_url)
+
         form = page.form_with(name: "daEnquiryForm")
         form['lodgeRangeType'] = 'on'
-        form['dateFrom'] = dateFrom
-        form['dateTo']   = dateTo
+        form['dateFrom'] = date_from.strftime('%d/%m/%Y')
+        form['dateTo']   = date_to.strftime('%d/%m/%Y')
         page = form.submit()
 
         headings = page.at('.bodypanel ~ div').search('h2')
