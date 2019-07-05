16c6df1fa42d0f487300d30a628526d8b9fe01d8
Matthew Landauer <mlandauer@gmail.com>
2019-07-06 02:02:51 +1000

    Extract method

--- lib/civica_scraper/page/index.rb
+++ lib/civica_scraper/page/index.rb
@@ -8,20 +8,24 @@ module CivicaScraper
         results = page.at("div.bodypanel ~ div")
 
         results.search("h4").each do |address|
-          d = address.next_sibling
-
-          council_reference = d.at("span[contains('Application No.')] ~ span").text
-          description = d.at("span[contains('Type of Work')] ~ span").text
-          date_received = d.at("span[contains('Date Lodged')] ~ span").text
+          fields = extract_fields(address.next_sibling)
 
           yield(
-            council_reference: council_reference,
+            council_reference: fields[:council_reference],
             address: address.text,
-            description: description,
-            date_received: Date.strptime(date_received, "%d/%m/%Y").to_s,
+            description: fields[:description],
+            date_received: Date.strptime(fields[:date_received], "%d/%m/%Y").to_s,
             url: (page.uri + address.at("a")["href"]).to_s
           )
         end
+      end
+
+      def self.extract_fields(div)
+        {
+          council_reference: div.at("span[contains('Application No.')] ~ span").text,
+          description: div.at("span[contains('Type of Work')] ~ span").text,
+          date_received: div.at("span[contains('Date Lodged')] ~ span").text
+        }
       end
     end
   end
