0ab2c512bb1527f5256ca559b213c6e3599361d0
Matthew Landauer <mlandauer@gmail.com>
2019-07-06 01:34:42 +1000

    Extract method

--- lib/civica_scraper/authority/nambucca.rb
+++ lib/civica_scraper/authority/nambucca.rb
@@ -1,6 +1,22 @@
 module CivicaScraper
   module Authority
     module Nambucca
+      def self.scrape_index(page)
+        headings = page.at('.bodypanel ~ div').search('h2')
+        (0..headings.size - 1).each do |i|
+          heading = headings[i]
+          d = heading.next_sibling
+          date_received = Date.strptime(d.at('span:contains("Date Lodged") ~ span').inner_text, '%d/%m/%Y').to_s rescue nil
+
+          yield(
+            council_reference: d.at('span:contains("Application No.") ~ span').inner_text,
+            address: heading.inner_text,
+            description: d.at('span:contains("Type of Work") ~ span').inner_text,
+            date_received: date_received
+          )
+        end
+      end
+
       def self.scrape_and_save
         date_from = Date.today - 10
         date_to = Date.today
@@ -13,22 +29,15 @@ module CivicaScraper
 
         page = Page::Search.period(page, date_from, date_to)
 
-        headings = page.at('.bodypanel ~ div').search('h2')
-        (0..headings.size - 1).each do |i|
-          heading = headings[i]
-          d = heading.next_sibling
-          date_received = Date.strptime(d.at('span:contains("Date Lodged") ~ span').inner_text, '%d/%m/%Y').to_s rescue nil
-
-          record = {
-            'council_reference' => d.at('span:contains("Application No.") ~ span').inner_text,
-            'address' => heading.inner_text,
-            'description' => d.at('span:contains("Type of Work") ~ span').inner_text,
+        scrape_index(page) do |record|
+          CivicaScraper.save(
+            'council_reference' => record[:council_reference],
+            'address' => record[:address],
+            'description' => record[:description],
             'info_url' => info_url,
             'date_scraped' => Date.today.to_s,
-            'date_received' => date_received
-          }
-
-          CivicaScraper.save(record)
+            'date_received' => record[:date_received]
+          )
         end
       end
     end
