2fd4cebeccd22ae965f7b6dc481735a5a40ea89f
Matthew Landauer <mlandauer@gmail.com>
2019-07-08 09:22:47 +1000

    Move AUTHORITIES to its own file

--- lib/civica_scraper.rb
+++ lib/civica_scraper.rb
@@ -4,6 +4,7 @@ require "civica_scraper/authority/woollahra"
 require "civica_scraper/page/detail"
 require "civica_scraper/page/index"
 require "civica_scraper/page/search"
+require "civica_scraper/authorities"
 require "civica_scraper/version"
 
 require "scraperwiki"
@@ -11,54 +12,6 @@ require "mechanize"
 
 # Scrape civica websites
 module CivicaScraper
-  AUTHORITIES = {
-    burwood: {
-      url: "https://ecouncil.burwood.nsw.gov.au/eservice/daEnquiryInit.do?doc_typ=10&nodeNum=219",
-      period: :last7days
-    },
-    wollondilly: {
-      url: "https://ecouncil.wollondilly.nsw.gov.au/eservice/daEnquiryInit.do?nodeNum=40801",
-      period: :last7days
-    },
-    woollahra: {},
-    nambucca: {
-      url:
-        "https://eservices.nambucca.nsw.gov.au/eservice/daEnquiryInit.do?doc_typ=10&nodeNum=2811",
-      period: :last10days
-    },
-    cairns: {
-      url: "https://eservices.cairns.qld.gov.au/eservice/daEnquiryInit.do?nodeNum=227",
-      period: :last30days
-    },
-    mount_gambier: {
-      url: "https://ecouncil.mountgambier.sa.gov.au/eservice/daEnquiryInit.do?nodeNum=21461",
-      period: :last2months
-    },
-    norwood: {
-      url: "https://ecouncil.npsp.sa.gov.au/eservice/daEnquiryInit.do?doc_typ=155&nodeNum=10209",
-      period: :lastmonth
-    },
-    tea_tree_gully: {
-      url: "https://www.ecouncil.teatreegully.sa.gov.au/eservice/daEnquiryInit.do?nodeNum=131612",
-      period: :lastmonth
-    },
-    loxton_waikerie: {
-      url: "https://eservices.loxtonwaikerie.sa.gov.au/eservice/daEnquiryInit.do?nodeNum=2811",
-      period: :lastmonth
-    },
-    orange: {
-      url: "https://ecouncil.orange.nsw.gov.au/eservice/daEnquiryInit.do?nodeNum=24",
-      period: :last30days
-    },
-    gawler: {
-      url: "https://eservices.gawler.sa.gov.au/eservice/daEnquiryInit.do?doc_typ=4&nodeNum=3228",
-      period: :lastmonth,
-      # Has an incomplete SSL chain: See
-      # https://www.ssllabs.com/ssltest/analyze.html?d=eservices.gawler.sa.gov.au
-      disable_ssl_certificate_check: true
-    }
-  }.freeze
-
   def self.scrape_and_save(authority)
     if authority == :woollahra
       Authority::Woollahra.scrape_and_save
---
+++ lib/civica_scraper/authorities.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module CivicaScraper
+  AUTHORITIES = {
+    burwood: {
+      url: "https://ecouncil.burwood.nsw.gov.au/eservice/daEnquiryInit.do?doc_typ=10&nodeNum=219",
+      period: :last7days
+    },
+    wollondilly: {
+      url: "https://ecouncil.wollondilly.nsw.gov.au/eservice/daEnquiryInit.do?nodeNum=40801",
+      period: :last7days
+    },
+    woollahra: {},
+    nambucca: {
+      url:
+        "https://eservices.nambucca.nsw.gov.au/eservice/daEnquiryInit.do?doc_typ=10&nodeNum=2811",
+      period: :last10days
+    },
+    cairns: {
+      url: "https://eservices.cairns.qld.gov.au/eservice/daEnquiryInit.do?nodeNum=227",
+      period: :last30days
+    },
+    mount_gambier: {
+      url: "https://ecouncil.mountgambier.sa.gov.au/eservice/daEnquiryInit.do?nodeNum=21461",
+      period: :last2months
+    },
+    norwood: {
+      url: "https://ecouncil.npsp.sa.gov.au/eservice/daEnquiryInit.do?doc_typ=155&nodeNum=10209",
+      period: :lastmonth
+    },
+    tea_tree_gully: {
+      url: "https://www.ecouncil.teatreegully.sa.gov.au/eservice/daEnquiryInit.do?nodeNum=131612",
+      period: :lastmonth
+    },
+    loxton_waikerie: {
+      url: "https://eservices.loxtonwaikerie.sa.gov.au/eservice/daEnquiryInit.do?nodeNum=2811",
+      period: :lastmonth
+    },
+    orange: {
+      url: "https://ecouncil.orange.nsw.gov.au/eservice/daEnquiryInit.do?nodeNum=24",
+      period: :last30days
+    },
+    gawler: {
+      url: "https://eservices.gawler.sa.gov.au/eservice/daEnquiryInit.do?doc_typ=4&nodeNum=3228",
+      period: :lastmonth,
+      # Has an incomplete SSL chain: See
+      # https://www.ssllabs.com/ssltest/analyze.html?d=eservices.gawler.sa.gov.au
+      disable_ssl_certificate_check: true
+    }
+  }.freeze
+end
