1a206b5566b826cf3426515abeef5b4e54eb70f0
Matthew Landauer <mlandauer@gmail.com>
2019-07-08 15:44:32 +1000

    Extract method

--- lib/civica_scraper.rb
+++ lib/civica_scraper.rb
@@ -12,10 +12,16 @@ require "mechanize"
 # Scrape civica websites
 module CivicaScraper
   def self.scrape_and_save(authority)
+    scrape(authority) do |record|
+      save(record)
+    end
+  end
+
+  def self.scrape(authority)
     raise "Unknown authority: #{authority}" unless AUTHORITIES.key?(authority)
 
     scrape_period(AUTHORITIES[authority]) do |record|
-      save(record)
+      yield(record)
     end
   end
 
