ec569babe1202905fdab627040cd0c17335c87b0
Matthew Landauer <mlandauer@gmail.com>
2019-07-08 10:51:48 +1000

    Extract method

--- lib/civica_scraper/page/detail.rb
+++ lib/civica_scraper/page/detail.rb
@@ -8,6 +8,19 @@ module CivicaScraper
       def self.scrape(doc)
         rows = doc.search(".rowDataOnly > .inputField:nth-child(2)").map { |e| e.inner_text.strip }
 
+        on_notice = extract_notification_period(doc)
+
+        {
+          council_reference: rows[2],
+          address: rows[0].squeeze(" "),
+          description: rows[1],
+          date_received: Date.strptime(rows[3], "%d/%m/%Y").to_s,
+          on_notice_from: (Date.strptime(on_notice[:from], "%d/%m/%Y").to_s if on_notice[:from]),
+          on_notice_to: (Date.strptime(on_notice[:to], "%d/%m/%Y").to_s if on_notice[:to])
+        }
+      end
+
+      def self.extract_notification_period(doc)
         table = doc.at("table[summary='Tasks Associated this Development Application']")
         table_row = table.search("tr").find do |tr|
           tr.search("td")[1]&.inner_text == "Notification to Neighbours"
@@ -17,14 +30,9 @@ module CivicaScraper
           on_notice_from = table_row.search("td")[2].inner_text
           on_notice_to = table_row.search("td")[3].inner_text
         end
-
         {
-          council_reference: rows[2],
-          address: rows[0],
-          description: rows[1],
-          date_received: Date.strptime(rows[3], "%d/%m/%Y").to_s,
-          on_notice_from: (Date.strptime(on_notice_from, "%d/%m/%Y").to_s if on_notice_from),
-          on_notice_to: (Date.strptime(on_notice_to, "%d/%m/%Y").to_s if on_notice_to)
+          from: on_notice_from,
+          to: on_notice_to
         }
       end
     end
