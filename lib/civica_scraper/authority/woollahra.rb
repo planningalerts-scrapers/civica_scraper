87dcc082790524d341a98d0470ce15e6ce204435
Matthew Landauer <mlandauer@gmail.com>
2019-07-08 10:17:08 +1000

    Improve alternate scraper for woollahra

--- lib/civica_scraper/authority/woollahra.rb
+++ lib/civica_scraper/authority/woollahra.rb
@@ -65,15 +65,21 @@ module CivicaScraper
         page = Page::Search.advertised(page)
 
         Page::Index.scrape(page) do |record|
+          # Now scrape the detail page so that we can get the notice information
+          page = agent.get(record[:url])
+          record_detail = Page::Detail.scrape(page)
+
           CivicaScraper.save(
-            "address" => record[:address],
-            "description" => record[:description],
-            "council_reference" => record[:council_reference],
+            "address" => record_detail[:address],
+            "description" => record_detail[:description],
+            "council_reference" => record_detail[:council_reference],
             # We can't give a link directly to an application.
             # Bummer. So, giving link to the search page
             "info_url" => info_url,
             "date_scraped" => Date.today.to_s,
-            "date_received" => record[:date_received]
+            "date_received" => record_detail[:date_received],
+            "on_notice_from" => record_detail[:on_notice_from],
+            "on_notice_to" => record_detail[:on_notice_to]
           )
         end
       end
--- lib/civica_scraper/page/detail.rb
+++ lib/civica_scraper/page/detail.rb
@@ -7,11 +7,24 @@ module CivicaScraper
     module Detail
       def self.scrape(doc)
         rows = doc.search(".rowDataOnly > .inputField:nth-child(2)").map { |e| e.inner_text.strip }
+
+        table = doc.at("table[summary='Tasks Associated this Development Application']")
+        table_row = table.search("tr").find do |tr|
+          tr.search("td")[1]&.inner_text == "Notification to Neighbours"
+        end
+
+        if table_row
+          on_notice_from = table_row.search("td")[2].inner_text
+          on_notice_to = table_row.search("td")[3].inner_text
+        end
+
         {
           council_reference: rows[2],
           address: rows[0],
           description: rows[1],
-          date_received: Date.strptime(rows[3], "%d/%m/%Y").to_s
+          date_received: Date.strptime(rows[3], "%d/%m/%Y").to_s,
+          on_notice_from: (Date.strptime(on_notice_from, "%d/%m/%Y").to_s if on_notice_from),
+          on_notice_to: (Date.strptime(on_notice_to, "%d/%m/%Y").to_s if on_notice_to)
         }
       end
     end
